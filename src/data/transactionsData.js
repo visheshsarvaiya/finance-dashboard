e4c78c867478e6eefdf9edab0c71801371a16016
YourGitHubUsername <your-email@example.com>
2026-04-03 17:32:29 +0530

    final commit

---
+++ src/data/transactionsData.js
@@ -0,0 +1,26 @@
+export const transactionsData = [
+  {
+    id: 1,
+    date: "Mar 31, 2026",
+    desc: "Business Income",
+    cat: "Business",
+    type: "income",
+    amt: 2647,
+  },
+  {
+    id: 2,
+    date: "Mar 30, 2026",
+    desc: "Bonus",
+    cat: "Salary",
+    type: "income",
+    amt: 1864,
+  },
+  {
+    id: 3,
+    date: "Mar 29, 2026",
+    desc: "Fast Food",
+    cat: "Food & Dining",
+    type: "expense",
+    amt: 351,
+  },
+];
