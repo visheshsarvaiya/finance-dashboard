ed2ab0bd4f48c25903b26338d761d8ff3e735f59
YourGitHubUsername <your-email@example.com>
2026-04-04 12:44:52 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    chunkSizeWarningLimit: 1000, // removes 500kb warning
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['recharts']
+        }
+      }
+    }
+  }
 })
